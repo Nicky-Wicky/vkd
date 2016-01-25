2799dbd234dbf43ac9af853b60bdc23b35ea356b
vi <yaremenko.v@gmail.com>
2016-01-25 20:58:12 +0200

    all btn

--- vkdownload.js
+++ vkdownload.js
@@ -1,9 +1,12 @@
 var btns_wrap = document.querySelectorAll('.play_btn_wrap');
 var btns = document.querySelectorAll('.play_new');
-var a = document.createElement('a');
-a.innerText = 'D';
-a.setAttribute('href', btns[0].parentNode.nextSibling.nextElementSibling.defaultValue);
-a.setAttribute('download', '');
-btns_wrap[0].appendChild(a);
-// пока это работает так: заходим в аудиозаписи, жмакаем ф12 и кописастим туда, должно на первую песню дать ссылку, жмакаем, качаем,
-// сейчас делаю на все кнопки
+
+for ( var i = 0; i < btns_wrap.length; i++ ) {
+  var a = document.createElement('a');
+  a.innerText = 'D';
+  a.setAttribute('href', btns[i].parentNode.nextSibling.nextElementSibling.defaultValue);
+  a.setAttribute('download', '');
+  btns_wrap[i].appendChild(a);
+}
+
+
